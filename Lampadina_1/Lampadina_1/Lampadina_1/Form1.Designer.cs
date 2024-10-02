b10f1f5a97b7dae2465a0102430c9eccadd1c1a7
Doha1809 <dohadarag@gmail.com>
2024-10-02 22:37:47 +0200

    Add files via upload

---
+++ Lampadina_1/Lampadina_1/Lampadina_1/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Lampadina_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            pictureBox1 = new PictureBox();
+            button6 = new Button();
+            button7 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Black;
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(698, 107);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "ACCENDI";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.WindowText;
+            button2.ForeColor = SystemColors.ControlLightLight;
+            button2.Location = new Point(698, 193);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 1;
+            button2.Text = "SPEGNI";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.White;
+            pictureBox1.Image = Properties.Resources.image__1___1_;
+            pictureBox1.Location = new Point(127, 27);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(534, 324);
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            pictureBox1.BackColorChanged += button1_Click;
+            pictureBox1.BackgroundImageChanged += button1_Click;
+            pictureBox1.Click += button1_Click;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.Black;
+            button6.ForeColor = SystemColors.ButtonHighlight;
+            button6.Location = new Point(12, 107);
+            button6.Name = "button6";
+            button6.Size = new Size(75, 23);
+            button6.TabIndex = 6;
+            button6.Text = "ALZA";
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.Black;
+            button7.ForeColor = SystemColors.ButtonHighlight;
+            button7.Location = new Point(12, 193);
+            button7.Name = "button7";
+            button7.Size = new Size(75, 23);
+            button7.TabIndex = 7;
+            button7.Text = "ABBASSA ";
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += button7_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(pictureBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private PictureBox pictureBox1;
+        private Button button6;
+        private Button button7;
+    }
+}
---
+++ Lampadina_1/Lampadina_1/Lampadina_1/Form1.cs
@@ -0,0 +1,58 @@
+using System.Drawing;
+
+
+namespace Lampadina_1
+{
+    public partial class Form1 : Form
+    {
+        private Image originalImage; // Variabile per salvare l'immagine originale
+        private int movementstep = 30;
+        public Form1()
+        {
+            InitializeComponent();
+            originalImage = pictureBox1.Image;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                Bitmap bitmap = new Bitmap(pictureBox1.Image);
+                ChangeImageColor(bitmap, Color.Yellow); // Cambia il colore con il colore desiderato
+                pictureBox1.Image = bitmap;
+            }
+        }
+
+        private void ChangeImageColor(Bitmap bitmap, Color newColor)
+        {
+            for (int x = 0; x < bitmap.Width - 115; x++)
+            {
+                for (int y = 0; y < bitmap.Height - 115; y++)
+                {
+                    Color originalColor = bitmap.GetPixel(x, y);
+                    // Cambia il colore del pixel
+                    if (originalColor.A > 0) // Solo per i pixel visibili
+                    {
+                        bitmap.SetPixel(x, y, newColor);
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = originalImage;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Top = movementstep;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Top = movementstep;
+        }
+    }
+}
+
